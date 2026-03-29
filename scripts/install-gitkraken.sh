c1cc741723cb67bc79582c3057c7c59b7dd689f5
Armando Ruiz <artmx@proton.me>
2026-03-29 11:55:47 -0600

    change scripts permissions

--- scripts/install-gitkraken.sh
+++ scripts/install-gitkraken.sh
@@ -53,7 +53,8 @@ install_gitkraken() {
   trap 'rm -rf "$TMP_DIR"' EXIT
 
   curl -L "$pkg_url" -o "$TMP_DIR/$pkg_name"
-  sudo apt install -y "$TMP_DIR/$pkg_name"
+  cd "$TMP_DIR"
+  sudo apt install -y "./$pkg_name"
 }
 
 # Ejecuta la función principal
