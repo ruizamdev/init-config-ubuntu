4d66006fad0e8f81cbb2657598bbf0d9d5e1a2f8
Armando Ruiz <artmx@proton.me>
2026-03-28 23:48:55 -0600

    ADD flatpak installation

--- scripts/install-utilities.sh
+++ scripts/install-utilities.sh
@@ -124,7 +124,7 @@ configure_nsswitch_winbind() {
 }
 
 install_flatpak() {
-  sudo apt install flatpak
+  sudo apt install -y flatpak
   sudo apt install gnome-software-plugin-flatpak
   flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 }
